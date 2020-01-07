d9a03e87b0cd3401c90d2515666132084080f5e1
Ricardo Fiel <rifiel@microsoft.com>
2020-01-07 23:28:47 +0000

    Added deploy to root

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 rgName=${1:-$(az group list --query [0].name -o tsv)}
 rgLocation=${2:-$(az group list --query [0].location -o tsv)} 
-templateFile=${3:-https://raw.githubusercontent.com/shoegazerpt/MCW-Serverless-architecture/btf2020/Hands-on%20lab/deploy/azureDeploy.json}
+templateFile="https://raw.githubusercontent.com/shoegazerpt/MCW-Serverless-architecture/btf2020/deploy/azureDeploy.json"
 tenantId=$(az account show --query tenantId -o tsv)
 userId=$(az ad signed-in-user show --query objectId -o tsv)
 
