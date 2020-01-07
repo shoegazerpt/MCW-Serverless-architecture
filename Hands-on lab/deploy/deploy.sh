20a763a94ad6f6167ee23367621535358d14bdc9
Ricardo Fiel <rifiel@microsoft.com>
2020-01-07 23:14:40 +0000

    fix template

--- Hands-on lab/deploy/deploy.sh
+++ Hands-on lab/deploy/deploy.sh
@@ -2,11 +2,12 @@
 #!/bin/bash
 rgName=${1:-$(az group list --query [0].name -o tsv)}
 rgLocation=${2:-$(az group list --query [0].location -o tsv)} 
-templateFile=${3:-}
-# tenantId=$(az account show --query tenantId -o tsv)
-# userId=$(az ad signed-in-user show --query objectId -o tsv)
+templateFile=${3:-https://raw.githubusercontent.com/shoegazerpt/MCW-Serverless-architecture/btf2020/Hands-on%20lab/deploy/azureDeploy.json}
+tenantId=$(az account show --query tenantId -o tsv)
+userId=$(az ad signed-in-user show --query objectId -o tsv)
 
-echo $rgName $rgLocation
+echo $rgName $rgLocation $templateFile
 
-# az group deployment create -n deploy -g $rgName --template-file $templateFile --parameters "{\"tenantId\":{\"value\":\"$tenantId\"},\"userId\":{\"value\":\"$userId\"}}"
+az group deployment create -n deploy -g $rgName --template-file $templateFile --parameters "{\"tenantId\":{\"value\":\"$tenantId\"},\"userId\":{\"value\":\"$userId\"}}"
 
+# functionapp settings
