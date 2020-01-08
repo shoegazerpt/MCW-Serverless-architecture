f329cd5ec8336d9bd4e34ee098f257ba3fc1ffa3
Ricardo Fiel <rifiel@microsoft.com>
2020-01-08 00:53:16 +0000

    function settings

--- Hands-on lab/deploy/deploy.sh
+++ Hands-on lab/deploy/deploy.sh
@@ -8,6 +8,24 @@ userId=$(az ad signed-in-user show --query objectId -o tsv)
 
 echo $rgName $rgLocation $templateUri
 
+echo ""
+echo "Creating deployment..."
 az group deployment create -n deploy -g $rgName --template-uri $templateUri --parameters "{\"tenantId\":{\"value\":\"$tenantId\"},\"userId\":{\"value\":\"$userId\"}}"
 
+echo ""
+echo "Getting outputs..."
+az group deployment show -n deploy -g $rgName --query properties.outputs > appsettings.json
+jq 'to_entries | map_values({ name: .key } + {slotSetting: false} + {value: .value.value})' appsettings.json > appsettings.txt
+#jq  -r 'to_entries|map("\(.key)=\(.value.value|tostring) ")|.[]' appsettings.json > appsettings.txt
+
+tollBoothFunctionName=$(jq -r '.[] | select(.name == "tollBoothFunctionName") | .value' appsettings.txt)
+
 # functionapp settings
+echo ""
+echo "Setting function appsettings..."
+az functionapp config appsettings set -g $rgName -n $tollBoothFunctionName --settings @appsettings.txt
+
+# functionapp restart
+echo ""
+echo "Restarting functions..."
+az functionapp restart -g $rgName -n $tollBoothFunctionName
